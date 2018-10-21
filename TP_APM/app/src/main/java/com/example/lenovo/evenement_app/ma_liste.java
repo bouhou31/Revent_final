3ea4b1685f6d91bd3ebe56d8b4e5854a63f77757
bouhou31 <ei_kechar@esi.dz>
2018-10-21 08:25:46 +0100

    Final Commit

---
+++ TP_APM/app/src/main/java/com/example/lenovo/evenement_app/ma_liste.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.evenement_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ma_liste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ma_liste);
+    }
+}
---
+++ TP_APM/app/src/main/java/com/example/lenovo/evenement_app/type_evenements.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.evenement_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class type_evenements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_evenements);
+    }
+}
